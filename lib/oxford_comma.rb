8aba74b0552e499cb2771ee6a697fcfacd0ffc10
Celeste Thomas <celestesmthomas@gmail.com>
2020-04-21 20:15:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  if array.length >= 1
+  if array.length <= 1
   array.join(" and ")
-  elsif array.length < 1
+  elsif array.length > 1
   array.insert(-2, "and")
 end
 end
